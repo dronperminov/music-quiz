38146e4b1b18b0280eb6b95db117764e32818f81
dronperminov <perminov@ispras.ru>
2024-08-18 17:55:36 +0300

    prettify balance input

--- web/js/inputs/balance_input.js
+++ web/js/inputs/balance_input.js
@@ -2,18 +2,30 @@ function BalanceInput(blockId, errorMessage, value, onChange = null) {
     this.block = document.getElementById(blockId)
     this.error = document.getElementById(`${blockId}-error`)
     this.errorMessage = errorMessage
+    this.onChange = onChange
+
+    this.modeCheckbox = document.getElementById(`${blockId}-mode`)
+    this.modeCheckbox.addEventListener("change", () => this.SwitchMode(true))
+    this.balanceBlock = this.block.getElementsByClassName("balance-input-balance-mode")[0]
+    this.checkboxBlock = this.block.getElementsByClassName("balance-input-checkbox-mode")[0]
 
     this.options = {}
     for (let track of this.block.getElementsByClassName("balance-input-option-track")) {
         let input = track.getElementsByTagName("input")[0]
         let name = input.getAttribute("data-name")
-        let value = document.getElementById(`${input.getAttribute("id")}-value`)
-        this.options[name] = {input: input, value: value}
+        let id = input.getAttribute("id")
+        let value = document.getElementById(`${id}-value`)
+        let checkbox = document.getElementById(`${id}-checkbox`)
 
-        input.addEventListener("input", () => this.UpdateValue(input, value))
+        this.options[name] = {input, value, checkbox}
 
-        if (onChange !== null)
+        input.addEventListener("input", () => this.UpdateValue(name))
+        checkbox.addEventListener("change", () => this.UpdateCheckbox(name))
+
+        if (onChange !== null) {
             input.addEventListener("change", () => onChange())
+            checkbox.addEventListener("change", () => onChange())
+        }
     }
 
     this.SetValue(value)
@@ -23,6 +35,7 @@ BalanceInput.prototype.SetValue = function(value) {
     for (let option of Object.values(this.options)) {
         option.input.value = 50
         option.value.innerText = "50"
+        option.checkbox.checked = true
     }
 
     if (value === null)
@@ -30,14 +43,19 @@ BalanceInput.prototype.SetValue = function(value) {
 
     let maxValue = 0
     let minValue = Infinity
+    let values = new Set()
 
     for (let optionValue of Object.values(value)) {
         maxValue = Math.max(maxValue, optionValue)
         minValue = Math.min(minValue, optionValue)
+        values.add(optionValue)
     }
 
-    if (maxValue == 0)
+    this.modeCheckbox.checked = values.size > 2 || values.size == 2 && minValue > 0
+
+    if (maxValue == 0) {
         maxValue = 1
+    }
     else if (minValue == maxValue)
         maxValue *= 2
 
@@ -45,7 +63,10 @@ BalanceInput.prototype.SetValue = function(value) {
         let optionValue = name in value ? Math.round(value[name] / maxValue * 1000) / 10 : 0
         option.input.value = optionValue
         option.value.innerText = `${optionValue}`
+        option.checkbox.checked = optionValue > 0
     }
+
+    this.SwitchMode()
 }
 
 BalanceInput.prototype.GetValue = function() {
@@ -74,7 +95,37 @@ BalanceInput.prototype.Clear = function() {
     this.SetValue(null)
 }
 
-BalanceInput.prototype.UpdateValue = function(input, value) {
-    value.innerText = input.value
+BalanceInput.prototype.UpdateValue = function(name) {
+    this.options[name].value.innerText = this.options[name].input.value
+    this.options[name].checkbox.checked = +this.options[name].input.value > 0
     this.error.innerText = ""
+}
+
+BalanceInput.prototype.UpdateCheckbox = function(name) {
+    let value = this.options[name].checkbox.checked ? 50 : 0
+    let changed = value != this.options[name].input.value
+    this.options[name].input.value = value
+    this.options[name].value.innerText = this.options[name].input.value
+    this.error.innerText = ""
+    return changed
+}
+
+BalanceInput.prototype.SwitchMode = function(withChange = false) {
+    let balanceMode = this.modeCheckbox.checked
+    let changed = false
+
+    if (balanceMode) {
+        this.balanceBlock.classList.remove("hidden")
+        this.checkboxBlock.classList.add("hidden")
+    }
+    else {
+        this.balanceBlock.classList.add("hidden")
+        this.checkboxBlock.classList.remove("hidden")
+
+        for (let name of Object.keys(this.options))
+            changed |= this.UpdateCheckbox(name)
+    }
+
+    if (withChange && changed)
+        this.onChange()
 }
