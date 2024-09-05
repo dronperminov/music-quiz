d1c2253953b87a59eebd191315cad193643bf39e
dronperminov <perminov@ispras.ru>
2024-09-05 08:09:52 +0300

    prettify notifications

--- web/js/utils/fetch.js
+++ web/js/utils/fetch.js
@@ -45,13 +45,20 @@ async function SendRequest(url, data = null) {
 }
 
 function ShowNotification(text, className = "error-notification", showTime = 2000) {
+    let notifications = document.getElementById("notifications")
+
+    if (notifications === null) {
+        notifications = document.createElement("div")
+        notifications.setAttribute("id", "notifications")
+        let body = document.getElementsByTagName("body")[0]
+        body.appendChild(notifications)
+    }
+
     let notification = document.createElement("div")
     notification.classList.add("notification")
     notification.classList.add(className)
     notification.innerHTML = text
-
-    let body = document.getElementsByTagName("body")[0]
-    body.appendChild(notification)
+    notifications.prepend(notification)
 
     setTimeout(() => {
         notification.classList.add("notification-open")
